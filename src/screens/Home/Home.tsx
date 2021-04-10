852cad5f6a5eebb105057e8c94f31f302a0995e4
Daniel <myworktime002@gmail.com>
2021-04-10 11:34:35 +0300

    setting social buttons on mobile layout

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -10,7 +10,12 @@ export const Home: React.FC = () => {
         src={VIDEO_DATA.HOME.src}
         type={VIDEO_DATA.HOME.format}
         style={{
+          zIndex: -1,
+          position: 'fixed',
+          right: 0,
+          minWidth: '100%',
           width: '100%',
+          minHeight: '100%',
           height: '100%',
         }}
       />
